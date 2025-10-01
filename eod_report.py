ae11928237b9ba17e520ed15d112e92b3c27e744
ukduw <edhkmayo@gmail.com>
2025-10-01 08:40:46 -0400

    added basic imports, env, pb + retry/dummy

--- eod_report.py
+++ eod_report.py
@@ -5,3 +5,32 @@
 
 # e.g. +5.0%, -1.6%, +3.4%...
     # total (additive): x%
+
+
+import time
+import requests
+
+from pushbullet import Pushbullet
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PB_API_KEY = os.getenv("PUSHBULLET_API_KEY")
+
+class DummyPB:
+    def push_note(self, title, body):
+        print(f"(PB Failed) Unsent notification: {title}, {body}")
+
+pb = DummyPB()
+
+pb_reconnect_tries = 0
+while pb_reconnect_tries < 5: # low due to risk of getting stuck in loop past premarket open...
+    try:
+        pb = Pushbullet(PB_API_KEY)
+        break
+    except requests.exceptions.ConnectionError as e:
+        pb_reconnect_tries += 1
+        print(f"PB connection failed({pb_reconnect_tries}/5), retrying in 10s...", e)
+        time.sleep(10)
