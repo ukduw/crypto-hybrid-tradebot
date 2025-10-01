25665263fa56791bd88cefd14a0f0561036cdb9f
ukduw <edhkmayo@gmail.com>
2025-10-01 08:37:56 -0400

    placeholder, plans

---
+++ eod_report.py
@@ -0,0 +1,7 @@
+# script to calculate and summarize daily p/l
+# write to log - needs systemd service/timer
+    # run at 23:30 (UTC)?
+# pushbullet noti
+
+# e.g. +5.0%, -1.6%, +3.4%...
+    # total (additive): x%
