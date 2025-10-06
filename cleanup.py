be00cfb44afa31aad0dcfd525d61dbbabd6a4eae
ukduw <edhkmayo@gmail.com>
2025-10-06 06:14:53 -0400

    refactored for crypto

--- cleanup.py
+++ cleanup.py
@@ -1,5 +1,5 @@
-from alpaca.data.live import StockDataStream
-from alpaca.data.enums import DataFeed
+from alpaca.data.live import CryptoDataStream
+from alpaca.data.enums import CryptoFeed
 
 from dotenv import load_dotenv
 import os
@@ -7,13 +7,13 @@ import json
 
 import asyncio
 
-from alpaca_utils import stop_price_quote_bar_stream
+from alpaca_utils import stop_price_bar_stream
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-stock_stream = StockDataStream(api_key=API_KEY, secret_key=SECRET_KEY, feed=DataFeed.SIP)
+crypto_stream = CryptoDataStream(api_key=API_KEY, secret_key=SECRET_KEY, feed=CryptoFeed.US)
 
 
 CONFIG_PATH = "configs.json"
@@ -22,14 +22,13 @@ with open("configs.json", "r") as f:
 
 symbols = [setup["symbol"] for setup in configs_json]
 
-# REFACTOR FOR CRYPTO
 
 async def main():
     print("Running systemd cleanup...")
 
     for symbol in symbols:
-        await stop_price_quote_bar_stream(symbol)
-    await stock_stream.stop_ws()
+        await stop_price_bar_stream(symbol)
+    await crypto_stream.stop_ws()
 
     tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
     for t in tasks:
